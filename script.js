4ecf8fdcf5d6e5ee0043c6f1cf01df683ab1432e
MiguelKoh <81209063+MiguelKoh@users.noreply.github.com>
2023-05-04 11:07:40 -0600

    4 columnas en pantalla grande

--- script.js
+++ script.js
@@ -5,31 +5,33 @@ const obtenerDatos = async () => {
     const respuestaProductos = await fetch("http://localhost/carrito/obtener_datos_producto.php");
     const productos = await respuestaProductos.json();
     console.log(productos);
-      let contador = 0;
-      let seccion = '';
+    let contador = 0;
+    let seccion = '';
 
-      productos.forEach(producto => {
-        contador++;
-        seccion += `
-          <div class="row">
-          <div class= "col-3 mt-4"> 
+    productos.forEach(producto => {
+      contador++;
+      if (contador % 4 === 1) {
+        seccion += '<div class="row">';
+      }
+      seccion += `
+        <div class="col-3 mt-4"> 
           <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="tienda/${producto.rutaimagen}" alt="Card image cap">
-              <div class="card-body">
+            <img class="card-img-top" src="tienda/${producto.rutaimagen}" alt="Card image cap">
+            <div class="card-body">
               <h5 class="card-title">${producto.nombre}</h5>
               <p class="card-text">${producto.precio}</p>
               <p class="card-text">Tallas: ${producto.tallas}</p>
               <a href="#" class="btn btn-primary">Agregar al carrito </a>
-              </div>
             </div>
-          </div>`
-          
-        if (contador % 3 === 0) {
-          seccion += `</div>`;
-        }
-        
-        seccionProductos.innerHTML = seccion;
-      });
+          </div>
+        </div>
+      `;
+      if (contador % 4 === 0 || contador === productos.length) {
+        seccion += '</div>';
+      }
+    });
+
+    seccionProductos.innerHTML = seccion;
   
   } catch (error) {
     console.error(error);
